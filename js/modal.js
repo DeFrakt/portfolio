7a54c4c9f6282998f75deb15b66fcda4c49b65fc
DeFrakt <syaussi@gmail.com>
2019-02-26 10:14:18 -0800

    mastering_api

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@ var emailModal = document.getElementById('emailModal');
 var resumeModal = document.getElementById('resumeModal');
 var beatModal = document.getElementById('beatModal');
 var foodieModal = document.getElementById('foodieModal');
+var masteringModal = document.getElementById('masteringModal');
 // Get the button that opens the modal
 var emailBtn = document.getElementById("emailBtn");
 var resumeBtn = document.getElementById("resumeBtn");
@@ -10,11 +11,13 @@ var project1Btn = document.getElementById("project1");
 var project2Btn = document.getElementById("project2");
 var project3Btn = document.getElementById("project3");
 var project4Btn = document.getElementById("project4");
+var project5Btn = document.getElementById("project5");
 // Get the <span> element that closes the modal
 var emailSpan = document.getElementsByClassName("emailClose")[0];
 var resumeSpan = document.getElementsByClassName("resumeClose")[0];
 var project1Span = document.getElementsByClassName("project1Close")[0];
 var project3Span = document.getElementsByClassName("project3Close")[0];
+var project5Span = document.getElementsByClassName("project5Close")[0];
 // EMAIL
 // When the user clicks the button, open the modal 
 emailBtn.onclick = function() {
@@ -61,6 +64,19 @@ project4Btn.onclick = function() {
 project3Span.onclick = function() {
     foodieModal.style.display = "none";
 }
+// Mastering
+// When the user clicks the button, open the modal 
+project5Btn.onclick = function() {
+    masteringModal.style.display = "block";
+}
+project5Btn.onclick = function() {
+    masteringModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+project5Span.onclick = function() {
+    masteringModal.style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -75,6 +91,9 @@ window.onclick = function(event) {
     }
     if (event.target == foodieModal) {
         foodieModal.style.display = "none";
+    }
+    if (event.target == masteringModal) {
+        masteringModal.style.display = "none";
     }
 }
 
